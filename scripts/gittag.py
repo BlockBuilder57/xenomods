4747f9ace5aef3d54e01d3416ac1bc2879ad0ffb
modeco80 <lily.modeco80@protonmail.ch>
2023-02-17 16:07:20 -0600

    gittag: Fix execution problems
    ${Python(3)_EXECUTABLE} wasn't exporting properly, so we use a generator expression to get the path of the interpreter target. This seems to work better.
    
    All the scripts were also given the execute bit.

--- scripts/gittag.py
+++ scripts/gittag.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # A simple Git version script.
 # Generates a C/C++ header with definitions.
--- scripts/makePackage.py
+++ scripts/makePackage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Hacky script for bf2mods packaging
 
