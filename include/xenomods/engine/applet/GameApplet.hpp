36a4a19f03a426424a98e685276bd90e7e87cc9e
BlockBuilder57 <newbrickbuilder7@gmail.com>
2023-05-27 21:38:39 -0500

    xm: Add all extra DE applets to headers

--- include/xenomods/engine/applet/GameApplet.hpp
+++ include/xenomods/engine/applet/GameApplet.hpp
@@ -1,13 +1,40 @@
 #pragma once
 
+#if XENOMODS_CODENAME(bfsw)
 namespace applet {
-	class GameTitle {
-	   public:
-		static void execute();
-	};
+	#define DEFINE_APPLET(T)       \
+		struct T {                 \
+		   public:                 \
+			static void execute(); \
+		};
 
-	class GameTitleMelia {
-	   public:
-		static void execute();
-	};
+	DEFINE_APPLET(GameTitle)
+	DEFINE_APPLET(GameTitleMelia)
+	DEFINE_APPLET(GameField)
+	DEFINE_APPLET(GameMelia)
+	DEFINE_APPLET(MessageSample)
+	DEFINE_APPLET(SampleApplet)
+	DEFINE_APPLET(SevViewer)
+	DEFINE_APPLET(TerrainApplet)
+	DEFINE_APPLET(HayanoAppletLauncher)
+	DEFINE_APPLET(HayanoApplet)
+	DEFINE_APPLET(HayanoAppletDevelopRpc)
+	DEFINE_APPLET(HayanoAppletLayer)
+	DEFINE_APPLET(HayanoAppletLayout)
+	DEFINE_APPLET(HayanoAppletSocket)
+	DEFINE_APPLET(InabaAnmPac)
+	DEFINE_APPLET(InabaColi)
+	DEFINE_APPLET(InabaEfbTest)
+	DEFINE_APPLET(InabaLayImg)
+	DEFINE_APPLET(InabaLayFont)
+	DEFINE_APPLET(InabaLayGrp)
+	DEFINE_APPLET(InabaMovie)
+	DEFINE_APPLET(InabaNavi)
+	DEFINE_APPLET(InabaOffScreen)
+	DEFINE_APPLET(KawashimaApplet)
+	DEFINE_APPLET(OohataTestApplet)
+	DEFINE_APPLET(OohataUITest)
+
+	#undef DEFINE_MANAGER
 } // namespace applet
+#endif
